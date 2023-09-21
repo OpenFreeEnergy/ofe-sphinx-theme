5b8c531e6ce186983d186836a85c6786668fd596
Josh Mitchell <yoshanuikabundi@gmail.com>
2023-09-21 15:59:28 +1000

    Move Autosummary templates to theme

--- ofe_sphinx_theme/__init__.py
+++ ofe_sphinx_theme/__init__.py
@@ -4,6 +4,7 @@
 from pathlib import Path
 
 from sphinx.application import Sphinx
+from sphinx.config import Config
 import sass
 
 from ._version import __version__
@@ -29,6 +30,8 @@ def compile_css(app: Sphinx, exception: Exception | None):
         output_style="compressed",
     )
 
+    print("Compiled css to bla")
+
     with open(dest, "w") as f:
         print(css, file=f)
 
@@ -38,6 +41,10 @@ def setup(app: Sphinx):
     app.add_html_theme("ofe_sphinx_theme", str(html_theme_path()))
 
     app.connect("build-finished", compile_css)
+
+    # usually this would be done in the `"config-init"` event,
+    # but this event runs before themes are loaded
+    app.config.templates_path.append(str(html_theme_path() / "templates"))
 
     return {
         "version": __version__,
